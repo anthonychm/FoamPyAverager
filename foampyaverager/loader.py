92c55d3ab7721191c55570abd46ade6cfbaba0bc
h81475am <anthony.man@manchester.ac.uk>
2025-11-22 20:27:34 +0000

    Call reader functions in main

--- foampyaverager/loader.py
+++ foampyaverager/loader.py
@@ -1,34 +1,28 @@
 import numpy as np
 
 class Reader:
-    def __init__(self, case_path, time, avg_var):
-        time_path = case_path + '/' + str(time) + '/'
+    def __init__(self, case_path, time):
+        self.time_path = case_path + '/' + str(time) + '/'
 
-        def read_file(file_name):
-            # Reads openfoam file
-            file = open(time_path + file_name)
-            setattr(self, file_name, file.read())
-            file.close()
+    def read_file(self, file_name):
+        # Reads openfoam file
+        file = open(self.time_path + file_name)
+        setattr(self, file_name, file.read())
+        file.close()
 
-        def extract_internal_field(file_name):
-            # Extracts internal field from an openfile file
-            start = getattr(self, file_name).find('(') + 1
-            end = getattr(self, file_name).find('boundaryField')
-            setattr(self, file_name, getattr(self, file_name)[start:end])
+    def extract_internal_field(self, file_name):
+        # Extracts internal field from an openfile file
+        start = getattr(self, file_name).find('(') + 1
+        end = getattr(self, file_name).find('boundaryField')
+        setattr(self, file_name, getattr(self, file_name)[start:end])
 
-        def remove_artifacts(file_name):
-            # Remove unwanted artifacts in the extracted internal field
-            setattr(self, file_name, getattr(self, file_name).replace('(', ''))
-            setattr(self, file_name, getattr(self, file_name).replace(')', ''))
-            setattr(self, file_name, getattr(self, file_name).replace(';', ''))
+    def remove_artifacts(self, file_name):
+        # Remove unwanted artifacts in the extracted internal field
+        setattr(self, file_name, getattr(self, file_name).replace('(', ''))
+        setattr(self, file_name, getattr(self, file_name).replace(')', ''))
+        setattr(self, file_name, getattr(self, file_name).replace(';', ''))
 
-        def convert_to_np(file_name):
-            # Convert openfoam string file into numpy array
-            arr = np.loadtxt(getattr(self, file_name).splitlines())
-            setattr(self, file_name, arr)
-
-        for var in ['Cx', 'Cy', 'Cz', avg_var]:
-            read_file(var)
-            extract_internal_field(var)
-            remove_artifacts(var)
-            convert_to_np(var)
+    def convert_to_np(self, file_name):
+        # Convert openfoam string file into numpy array
+        arr = np.loadtxt(getattr(self, file_name).splitlines())
+        setattr(self, file_name, arr)
--- foampyaverager/main.py
+++ foampyaverager/main.py
@@ -4,8 +4,14 @@ Run this file to execute domain averaging in an OpenFOAM case
 import loader
 
 def main(case_path, time, avg_var, avg_dir):
-    reader = loader.Reader(case_path, time, avg_var)
 
+    # Load openfoam files
+    reader = loader.Reader(case_path, time)
+    for var in ['Cx', 'Cy', 'Cz', avg_var]:
+        reader.read_file(var)
+        reader.extract_internal_field(var)
+        reader.remove_artifacts(var)
+        reader.convert_to_np(var)
 
 if __name__ == '__main__':
     main("../examples/Re_tau945_RANS_M3.64m", 100000, "U", "z")
