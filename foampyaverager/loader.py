98c3759dc099be88194bcfe8dd9d508e81aff1f0
h81475am <anthony.man@manchester.ac.uk>
2025-11-23 22:00:52 +0000

    Use C for coords and change file_name to var

--- foampyaverager/loader.py
+++ foampyaverager/loader.py
@@ -4,25 +4,25 @@ class Reader:
     def __init__(self, case_path, time):
         self.time_path = case_path + '/' + str(time) + '/'
 
-    def read_file(self, file_name):
+    def read_file(self, var):
         # Reads openfoam file
-        file = open(self.time_path + file_name)
-        setattr(self, file_name, file.read())
+        file = open(self.time_path + var)
+        setattr(self, var, file.read())
         file.close()
 
-    def extract_internal_field(self, file_name):
+    def extract_internal_field(self, var):
         # Extracts internal field from an openfile file
-        start = getattr(self, file_name).find('(') + 1
-        end = getattr(self, file_name).find('boundaryField')
-        setattr(self, file_name, getattr(self, file_name)[start:end])
+        start = getattr(self, var).find('(') + 1
+        end = getattr(self, var).find('boundaryField')
+        setattr(self, var, getattr(self, var)[start:end])
 
-    def remove_artifacts(self, file_name):
+    def remove_artifacts(self, var):
         # Remove unwanted artifacts in the extracted internal field
-        setattr(self, file_name, getattr(self, file_name).replace('(', ''))
-        setattr(self, file_name, getattr(self, file_name).replace(')', ''))
-        setattr(self, file_name, getattr(self, file_name).replace(';', ''))
+        setattr(self, var, getattr(self, var).replace('(', ''))
+        setattr(self, var, getattr(self, var).replace(')', ''))
+        setattr(self, var, getattr(self, var).replace(';', ''))
 
-    def convert_to_np(self, file_name):
+    def convert_to_np(self, var):
         # Convert openfoam string file into numpy array
-        arr = np.loadtxt(getattr(self, file_name).splitlines())
-        setattr(self, file_name, arr)
+        arr = np.loadtxt(getattr(self, var).splitlines())
+        setattr(self, var, arr)
