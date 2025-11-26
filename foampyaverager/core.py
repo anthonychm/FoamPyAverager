d3ac284078a5926bd5dd471ad7ce9521910ad7c4
h81475am <anthony.man@manchester.ac.uk>
2025-11-26 00:08:24 +0000

    Get indexes of unique coordinates

---
+++ foampyaverager/core.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class Averager:
+    def __init__(self, reader, avg_dirs):
+        for name, value in reader.__dict__.items():
+            setattr(self, name, value)
+        self.avg_dirs = avg_dirs
+        self.dirs_dict = {'x': 0, 'y': 1, 'z': 2}
+        self.avg_col_idx = [self.dirs_dict[d] for d in self.avg_dirs]
+
+    def get_unique_coords(self):
+        # Get unique coordinates in the non-averaging directions
+        self.non_avg_C = np.delete(self.C, self.avg_col_idx, axis=1)
+        self.unique_coords = np.unique(self.non_avg_C, axis=0)
+
+    def create_unique_coords_dict(self):
+        return {tuple(c): None for c in self.unique_coords}
+
+    def get_coords_indexes(self):
+        # Get a list of indexes for each unique coordinate
+        self.unique_coords_idx = self.create_unique_coords_dict()
+        for c in self.unique_coords_idx.keys():
+            C_view = self.non_avg_C.view([('', self.non_avg_C.dtype)] * self.non_avg_C.shape[1])
+            c_view = np.array(c).view([('', self.non_avg_C.dtype)] * self.non_avg_C.shape[1])
+            self.unique_coords_idx[c] = np.where(C_view == c_view)[0]
+
+    def calc_averaged(self):
+        # Calculate averaged values of the averaging variable
+        self.unique_coords_avg = self.create_unique_coords_dict()
