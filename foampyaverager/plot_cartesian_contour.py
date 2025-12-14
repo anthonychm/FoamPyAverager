237c4f251307752b323b461e5eda2d8fcc28c256
h81475am <anthony.man@manchester.ac.uk>
2025-12-14 20:02:12 +0000

    Add argparse to plot cartesian contour file

--- foampyaverager/plot_cartesian_contour.py
+++ foampyaverager/plot_cartesian_contour.py
@@ -2,15 +2,16 @@
 Run this file to plot a contour of a two-dimensional domain-averaged result
 """
 
-from foampyaverager import loader
-from contour_plotter import CartesianContourPlotter
+import ast
+import argparse
+from foampyaverager import loader, contour_plotter
 
-def main(pth, unique_coords_file, avg_var_file, avg_var_col, x_ticks, y_ticks, c_levels, cmap):
+def main(path, unique_coords_file, avg_var_file, avg_var_col, x_ticks, y_ticks, c_levels, cmap):
     # Get unique coordinates and averaged variable data
-    reader = loader.AveragedReader(pth, unique_coords_file, avg_var_file, avg_var_col)
+    reader = loader.AveragedReader(path, unique_coords_file, avg_var_file, avg_var_col)
 
     # Create domain and averaged variable grids
-    ccp = CartesianContourPlotter(reader.unique_coords)
+    ccp = contour_plotter.CartesianContourPlotter(reader.unique_coords)
     ccp.create_grids()
     ccp.fill_gridv(reader.averaged_var)
 
@@ -19,5 +20,22 @@ def main(pth, unique_coords_file, avg_var_file, avg_var_col, x_ticks, y_ticks, c
 
 
 if __name__ == "__main__":
-    main("", "unique_Cx_Cy", "averaged_U", 1, [-0.4, -0.38, -0.36, -0.34, -0.32, -0.30, -0.28, -0.26, -0.24],
-         [0, 0.005, 0.010, 0.015, 0.020, 0.025, 0.030, 0.035, 0.040], [0, 2, 4, 6, 8, 10, 12, 14, 16], 'plasma')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", required=True, type=str)
+    parser.add_argument("--unique_coords_file", required=True, type=str)
+    parser.add_argument("--avg_var_file", required=True, type=str)
+    parser.add_argument("--avg_var_col", required=True, type=int)
+    parser.add_argument("--x_ticks", required=True)
+    parser.add_argument("--y_ticks", required=True)
+    parser.add_argument("--c_levels", required=True)
+    parser.add_argument("--cmap", required=True, type=str)
+    args = parser.parse_args()
+
+    x_ticks = ast.literal_eval(args.x_ticks)
+    y_ticks = ast.literal_eval(args.y_ticks)
+    c_levels = ast.literal_eval(args.c_levels)
+
+    main(args.path, args.unique_coords_file, args.avg_var_file, args.avg_var_col,
+         x_ticks, y_ticks, c_levels, args.cmap)
+    # main("", "unique_Cx_Cy", "averaged_U", 1, [-0.4, -0.38, -0.36, -0.34, -0.32, -0.30, -0.28, -0.26, -0.24],
+    #      [0, 0.005, 0.010, 0.015, 0.020, 0.025, 0.030, 0.035, 0.040], [0, 2, 4, 6, 8, 10, 12, 14, 16], 'plasma')
