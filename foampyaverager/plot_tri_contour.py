9cf20eb85416f4b989a5d48d5519a233810a01f2
h81475am <anthony.man@manchester.ac.uk>
2026-01-17 18:53:48 +0000

    Add args reader to tri contour plotter

--- foampyaverager/plot_tri_contour.py
+++ foampyaverager/plot_tri_contour.py
@@ -18,6 +18,14 @@ def main(path, unique_coords_file, avg_var_file, avg_var_col,
 
 
 if __name__ == "__main__":
-    main("", "unique_Cx_Cy", "averaged_U", 1, [-0.4, -0.38, -0.36, -0.34, -0.32, -0.30, -0.28, -0.26, -0.24],
-         [0, 0.005, 0.010, 0.015, 0.020, 0.025, 0.030, 0.035, 0.040], [0, 2, 4, 6, 8, 10, 12, 14, 16],
-         'plasma', 'test.png')
+    args_reader = loader.ArgsReader.ContourArgsReader()
+    args_reader.parse_args()
+    args_reader.eval_ticks()
+    main(args_reader.args.path, args_reader.args.unique_coords_file,
+         args_reader.args.avg_var_file, args_reader.args.avg_var_col,
+         args_reader.x_ticks, args_reader.y_ticks, args_reader.c_levels,
+         args_reader.args.cmap, args_reader.args.save_name)
+
+    # main("", "unique_Cx_Cy", "averaged_U", 1, [-0.4, -0.38, -0.36, -0.34, -0.32, -0.30, -0.28, -0.26, -0.24],
+    #      [0, 0.005, 0.010, 0.015, 0.020, 0.025, 0.030, 0.035, 0.040], [0, 2, 4, 6, 8, 10, 12, 14, 16],
+    #      'plasma', 'test.png')
