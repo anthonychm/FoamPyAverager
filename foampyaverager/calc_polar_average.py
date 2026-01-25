35700ec83dd10a3ad51e60ec6ebdbfbc0454c821
h81475am <anthony.man@manchester.ac.uk>
2026-01-25 18:46:36 +0000

    Initial version of polar average calculator script

---
+++ foampyaverager/calc_polar_average.py
@@ -0,0 +1,31 @@
+"""
+Run this file to execute domain averaging
+in cylindrical coordinates for an OpenFOAM case
+"""
+
+from foampyaverager import loader, core
+
+def main(case_path, time, avg_var, origin, ax_col_dir, n_unique_radii):
+
+    # Load openfoam files
+    reader = loader.OpenFoamReader(case_path, time)
+    for var in ['C', avg_var]:
+        reader.read_file(var)
+        reader.extract_internal_field(var)
+        reader.remove_artifacts(var)
+        reader.convert_to_np(var)
+
+    # Convert cartesian coordinates to polar coordinates
+    averager = core.PolarAverager(reader, origin, ax_col_dir)
+    averager.get_polar_coords()
+
+    # Perform averaging
+    averager.get_unique_radii(n_unique_radii)
+    averager.calc_averaged(avg_var)
+
+    # Write unique radii and averaged variable
+    averager.write_unique_radii()
+    averager.write_averaged(avg_var)
+
+if __name__ == '__main__':
+    main("../examples/pipeCyclic", 227, "U", [0, 0], "x", 5)
